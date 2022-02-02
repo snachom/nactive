689ec0446fd6214504c737f146a3568a58f5b258
snachom <snachom@github.com>
2022-02-02 08:43:36 +0100

    Changes in urls buildv

--- src/pages/home/Homepage.js
+++ src/pages/home/Homepage.js
@@ -5,7 +5,7 @@ export const Homepage = () => {
   return (  
     <div className="homepage app-container container">
       home
-      <img src="/blob/main/assets/dc-batman.jpg" width="300" height="300" alt="home" />
+      <img src="https://github.com/snachom/nactive/blob/main/docs/assets/dc-batman.jpg" width="300" height="300" alt="home" />
 
     </div>
   )
