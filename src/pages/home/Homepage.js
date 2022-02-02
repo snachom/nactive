4f85e52e2c6965d9f68f11302cce64eebe1a8ca4
snachom <snachom@github.com>
2022-02-02 07:21:09 +0100

    Testing build

--- src/pages/home/Homepage.js
+++ src/pages/home/Homepage.js
@@ -3,6 +3,10 @@ import React from 'react';
 export const Homepage = () => {
 
   return (  
-    <div className="homepage app-container container">home</div>
+    <div className="homepage app-container container">
+      home
+      <img src="./assets/dc-batman.jpg" alt="home" />
+
+    </div>
   )
 };
