189a577dd4d2a2db4626766a4114f5614c3c6a7a
snachom <salas.ignacio@hotmail.com>
2022-02-02 19:40:09 +0100

    changes in home for testing

--- src/pages/home/Homepage.js
+++ src/pages/home/Homepage.js
@@ -5,7 +5,7 @@ export const Homepage = () => {
   return (  
     <div className="homepage app-container container">
       home
-      <img src="https://snachom.github.io/nactive/assets/dc-batman.jpg" width="300" height="400" alt="home" />
+      <img src="/nactive/assets/dc-batman.jpg" width="300" height="400" alt="home" />
       {/* https://snachom.github.io/nactive/assets/dc-batman.jpg */}
     </div>
   )
